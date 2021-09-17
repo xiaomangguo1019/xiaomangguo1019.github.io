f7856090cc4d4e522cd49c4a58909acbfa2eb4d1
sunyueyue <1280441233@qq.com>
2021-09-17 09:53:58 +0800

    first vuepress

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  title: 'Flomesh',  // 设置网站标题
+  description : 'Adroi',
+  base : '/yue/',
+  themeConfig : {
+	navbar:false,
+    nav : [
+        { text: ' ', link: '/apiword' }
+    ],  
+    sidebar: 'auto'
+  }
+}
---
+++ docs/.vuepress/theme/components/Page.vue
@@ -0,0 +1,49 @@
+<template>
+  <main class="page">
+    <slot name="top" />
+
+    <Content class="theme-default-content" />
+    <PageEdit />
+
+    <PageNav v-bind="{ sidebarItems }" />
+
+    <slot name="bottom" />
+  </main>
+</template>
+
+<script>
+import PageEdit from '@theme/components/PageEdit.vue'
+import PageNav from '@theme/components/PageNav.vue'
+
+export default {
+  components: { PageEdit, PageNav },
+  props: ['sidebarItems'],
+   mounted() {
+  
+    import("mermaid/dist/mermaid").then(m => {
+      m.initialize({
+        startOnLoad: true
+      });
+      m.init();
+    });
+  },
+  updated () {
+      
+    import("mermaid/dist/mermaid").then(m => {
+      m.initialize({
+        startOnLoad: true
+      });
+      m.init();
+    });
+  }
+}
+</script>
+
+<style lang="stylus">
+@require '../styles/wrapper.styl'
+
+.page
+  padding-bottom 2rem
+  display block
+
+</style>
